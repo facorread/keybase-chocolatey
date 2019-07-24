e75dfb9bf32cbb3bdb3178b17eb076dc7942e68d
Fabio Correa <facorread@gmail.com>
2019-07-24 08:11:31 -0400

    Original source code and logo

---
+++ README.md
@@ -0,0 +1,13 @@
+To build this package, type
+
+```bash
+cd src
+choco pack
+```
+
+# Credits
+
+I fetched the code for this package from `https://github.com/ferventcoder/chocolatey-packages/tree/d997b7ef21c2cbe753c881396da3282ac8926d88/manual/keybase` on Wednesday, July 24, 2019.
+
+I fetched icon_256x256.png from `https://github.com/keybase/client/blob/85705e8219fec9e8a91c996bb5790def1791884c/media/icons/Keybase.iconset/icon_256x256.png` on Wednesday, July 24, 2019.
+
---
+++ src/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+# Currently, version number information is taking from here:
+# https://s3.amazonaws.com/prerelease.keybase.io/update-windows-prod-v2.json
+
+$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  softwareName           = 'Keybase*'
+  FileType               = 'msi'
+  SilentArgs             = "/qn /norestart /l*v `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`""
+  url                    = 'https://prerelease.keybase.io/windows/Keybase_4.0.0-20190507155043%2B6614a49937.amd64.msi'
+  checksum               = 'A7F765D838C449A5B789FD1D4E78E2DA49EC4F1108519827651CF9058E98BCB4'
+  checksumType           = 'sha256'
+  validExitCodes         = @(0,1641,3010)
+}
+
+Install-ChocolateyPackage @packageArgs
