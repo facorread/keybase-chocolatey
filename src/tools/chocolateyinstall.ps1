beddd2d351bcfa3a94d588e3ebf9a36f3ff1fd4e
Fabio Correa <facorread@gmail.com>
2019-08-12 14:31:23 -0400

    Keybase 4.3.0

--- src/tools/chocolateyinstall.ps1
+++ src/tools/chocolateyinstall.ps1
@@ -1,15 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-# Currently, version number information is taking from here:
+# URL and SHA256 checksum come from
 # https://s3.amazonaws.com/prerelease.keybase.io/update-windows-prod-v2.json
+# Subscribe to updates on this file on https://www.wachete.com
 
 $packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
   softwareName           = 'Keybase*'
   FileType               = 'msi'
   SilentArgs             = "/qn /norestart /l*v `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`""
-  url                    = 'https://prerelease.keybase.io/windows/Keybase_4.2.0-20190710141157%2Be7c0bdc4a2.amd64.msi'
-  checksum               = '02a759ec9c70a21d27b4b10eb5d5ccd2e61ea00eeec48480ba7b44641905ff04'
+  url                    = 'https://prerelease.keybase.io/windows/Keybase_4.3.0-20190809132440%2Bf8de90a10f.amd64.msi'
+  checksum               = 'f61655dbdc9b2c33e5cddb05cb34ee698deba5ebf2145f0b43934f8332e1cf80'
   checksumType           = 'sha256'
   validExitCodes         = @(0,1641,3010)
 }
