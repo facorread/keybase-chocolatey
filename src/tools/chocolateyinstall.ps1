1f9d92322e5ad437958728d1896242d3ff6b87fa
Fabio A. Correa Duran <facorread@gmail.com>
2019-09-10 18:46:23 -0400

    Keybase 4.4.1

--- src/tools/chocolateyinstall.ps1
+++ src/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $packageArgs = @{
   softwareName           = 'Keybase*'
   FileType               = 'msi'
   SilentArgs             = "/qn /norestart /l*v `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`""
-  url                    = 'https://prerelease.keybase.io/windows/Keybase_4.4.0-20190829143953%2B8f13068775.amd64.msi'
-  checksum               = 'e88086c0d70545775e6c95c3782bf267dc8c46f1d121a54d8c4b922fbbc5fa5f'
+  url                    = 'https://prerelease.keybase.io/windows/Keybase_4.4.1-20190909144547%2Bb5f8897c2a.amd64.msi'
+  checksum               = 'de58f75fd7dc46a9e748be70b3ce2ffabcd19bf7dfac14a4f78a1c22bfddcd62'
   checksumType           = 'sha256'
   validExitCodes         = @(0,1641,3010)
 }
