d6dd0435c34339f503105d6890ca6ec54cd6ad9a
Fabio A. Correa Duran <facorread@gmail.com>
2020-05-27 16:01:45 -0400

    5.5.0

--- src/tools/chocolateyinstall.ps1
+++ src/tools/chocolateyinstall.ps1
@@ -5,14 +5,14 @@
 # Subscribe to updates on this file on https://www.wachete.com
 
 $packageArgs = @{
-  packageName            = "$env:chocolateyPackageName"
-  softwareName           = 'Keybase*'
-  FileType               = 'msi'
-  SilentArgs             = "/qn /norestart /l*v `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`""
-  url                    = 'https://prerelease.keybase.io/windows/Keybase_5.4.2-20200424164159%2B7b0bbf1e3c.amd64.msi'
-  checksum               = 'f1c74cbcc82abf3743a7334575616f3cc724adbcd12c44da9a235e43e060be92'
-  checksumType           = 'sha256'
-  validExitCodes         = @(0,1641,3010)
+  packageName    = "$env:chocolateyPackageName"
+  softwareName   = 'Keybase*'
+  FileType       = 'msi'
+  SilentArgs     = "/qn /norestart /l*v `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`""
+  url            = 'https://prerelease.keybase.io/windows/Keybase_5.5.0-20200526130944%2B139bb348af.amd64.msi'
+  checksum       = 'e7bb934ee975142d6225f7117c395b7892429fa6224d9b968634487849c7bf7b'
+  checksumType   = 'sha256'
+  validExitCodes = @(0, 1641, 3010)
 }
 
 Install-ChocolateyPackage @packageArgs
