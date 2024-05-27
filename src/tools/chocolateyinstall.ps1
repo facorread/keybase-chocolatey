395ac2963402bbeb79035e3cecce21fa7d3ae4f0
Fabio A. Correa <facorread@gmail.com>
2024-05-27 13:03:16 -0400

    6.2.8

--- src/tools/chocolateyinstall.ps1
+++ src/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $packageArgs = @{
   softwareName   = 'Keybase*'
   FileType       = 'msi'
   SilentArgs     = "/qn /norestart /l*v `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`""
-  url            = "https://prerelease.keybase.io/windows/Keybase_6.2.5-20231231232650%2B3bdf76f84a.amd64.msi"
-  checksum       = "9b088a8a743c6a742310f931c0fcd501ccc0502bb629614faebcb29813e3f945"
+  url            = "https://prerelease.keybase.io/windows/Keybase_6.2.8-20240305122718%2B5381dd5b22.amd64.msi"
+  checksum       = "4d75f6b825a2b08d384387f6520743d1a2a875cc618d52e47598803d21fcf58a"
   checksumType   = 'sha256'
   validExitCodes = @(0, 1641, 3010)
 }
